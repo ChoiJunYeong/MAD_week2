7a5ae520dc59c1d29609135462ca4757f8660484
dahye <k2dh007@naver.com>
2018-07-11 15:48:03 +0900

    아마도 최종?

--- app/src/main/java/com/example/q/a2ndweek/EndPoints.java
+++ app/src/main/java/com/example/q/a2ndweek/EndPoints.java
@@ -4,7 +4,7 @@ public class EndPoints {
     public static final String ROOT_URL = "http://52.231.65.151:8080/add-post";
     public static final String UPLOAD_URL = ROOT_URL + "uploadpic";
     public static final String UPROAD_PROFILE_PIC = "http://52.231.65.151:8080/update-profile-image";
-    public static final String DOWNROAD_PROFILE_PIC = "http://52.231.65.151:8080/get-profile-image/";
+    public static final String DOWNROAD_PROFILE_PIC = "http://52.231.65.151:8080/profile-image/";
     public static final String POST_URL = "http://52.231.65.151:8080/post/";
     public static final String GET_PICS_URL ="http://52.231.65.151:8080/post/images/";
 }
